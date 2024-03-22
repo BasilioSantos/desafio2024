db2e2d2988441eca841320ac1ab88e0ef393be24
Basilio <junior.basilio@gmail.com>
2024-03-21 23:07:41 -0300

    adequação as instruções do desafio

--- controllers/manipulaStringController.js
+++ controllers/manipulaStringController.js
@@ -2,12 +2,11 @@ const manipulaStringService = require('../services/manipulaStringService');
 
 module.exports = async (req, res) => {
   try {
-  console.log('req.body ',req.body)
     const { texto } = req.body 
 
-    if (!texto) {
-        return res.status(400).json({ error: 'Texto inválido' });
-    }
+    if (!texto || typeof texto !== 'string') {
+      return res.status(400).json({ error: 'Texto inválido! informe um texto válido! ex: {texto: xxxx}' });
+  }
 
       const palindromo = manipulaStringService.palindromo(texto);
       const ocorrencias_caracteres = manipulaStringService.quantidadePorCaracter(texto)
@@ -15,7 +14,7 @@ module.exports = async (req, res) => {
 
         return res.json(retorno);
     } catch (error) {
-        console.error('Erro ao mainuplar o texto:', error);
+        console.error('Erro ao manipular o texto:', error);
         return res.status(500).json({ error: 'Erro ao processar o texto' });
     }
 };
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,43 @@
+/* AVISO 
+ As linhas 8 e 12 foram comentadas para adequação pois uma das instruções é:
+ "implemente as operações de manipulação de string dentro do método POST"
+ caso estas linhas sejam "descomentadas" e o segundo metodo post apagado termos uma 
+ outra abordagem de desenvolvimento.
+*/  
 
-
-const manipulaStringController = require('../controllers/manipulaStringController');
+//const manipulaStringController = require('../controllers/manipulaStringController');
 
 module.exports = (app)=>{
 
-  app.post('/api/manipulacao-string', manipulaStringController)
+  //app.post('/api/manipulacao-string', manipulaStringController)
+  
+  app.post('/api/manipulacao-string', async (req,res)=>{
+
+    const { texto } = req.body 
+
+      if (!texto || typeof texto !== 'string') {
+          return res.status(400).json({ error: 'Texto inválido! informe um texto válido! ex: {texto: xxxx}' });
+      }
+  
+    const reverseString = (texto)=> {
+      if (texto === "")
+        return "";
+      else
+        return reverseString( texto.substr(1) ) + texto.charAt(0);
+    }
+          
+      const contador = {};
+      for (let i = 0; i < texto.length; i++) {
+          const c = texto.charAt(i);    
+          contador[c] = contador[c] ? contador[c]+1 : 1 
+      }
+  
+        const palindromo = reverseString(texto)==texto ? true : false
+        const ocorrencias_caracteres = contador
+        const retorno = {palindromo, ocorrencias_caracteres }
+  
+          return res.json(retorno);
+
+  })
  
 }
