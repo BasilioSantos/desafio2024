2c7fa6b4fa3c7a434ecbd9e14ca138baa4b65e5b
Basilio <junior.basilio@gmail.com>
2024-03-22 10:40:23 -0300

    retirada de alguns comentarios

--- api.js
+++ api.js
@@ -2,24 +2,10 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 
-//const compression = require('compression')
-
 const routes = require('./routes')
-//app.use(compression())
-app.use(bodyParser.urlencoded({
-  extended: true,
-  limit: '100mb'
-}));
 
-app.use(bodyParser.json({
-  limit: '100mb'
-}));
-//const router = express.Router();
+app.use(bodyParser.json());
 routes(app)
-
-//app.use('/teste/', routes)
-
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
